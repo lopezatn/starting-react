06d64352d2cc5e5874b84e76bc854b4db28885ac
Agustin <agustinlpzb@gmail.com>
2022-10-30 16:23:51 +0100

    Updates

---
+++ precache-manifest.b8ca89e965168d215ebff5e6b096dc1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e394823c7651c4feacd",
+    "url": "/starting-react/static/css/main.10dab79f.chunk.css"
+  },
+  {
+    "revision": "4e394823c7651c4feacd",
+    "url": "/starting-react/static/js/main.4e394823.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/starting-react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/starting-react/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "c3813ce0ca4262c77ac6",
+    "url": "/starting-react/static/js/runtime~main.c3813ce0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/starting-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1deb55ff714131ab966d926560170c2a",
+    "url": "/starting-react/index.html"
+  }
+];
